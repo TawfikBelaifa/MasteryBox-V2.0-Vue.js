290d0ca0c8cef0af5c9dce88aeff29a2e396f7ed
TawfikBelaifa <tawfik.professionnel@gmail.com>
2021-08-05 18:42:08 +0200

    header app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,85 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="top">
+      <div class="mini-nmeSite">
+        <h1>Mystery Box</h1>
+      </div>
+      <nav class="navigation">
+        <ul class="JiiH">
+          <router-link to="/">
+            <li>Accueil</li>
+          </router-link>
+          <router-link to="/Tous">
+            <li>Tous</li>
+          </router-link>
+          <router-link to="/Femmes">
+            <li>Femmes</li>
+          </router-link>
+          <router-link to="/Homme">
+            <li>Homme</li>
+          </router-link>
+          <router-link to="/Panier">
+            <li class="entour">Panier</li>
+          </router-link>
+        </ul>
+      </nav>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
+  width: 100vw;
+  padding: 0;
+  margin: 0;
+}
+
+.top {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 10px;
+  z-index: 1;
+  background-color: #000;
+
+  a {
+    text-decoration: none;
+  }
+
+  .mini-nmeSite h1 {
+    font-family: "Merienda", cursive;
+    font-size: 14px;
+    font-weight: 100;
+    color: bisque;
+    text-transform: uppercase;
+  }
+
+  .JiiH {
+    display: flex;
+    list-style: none;
+    padding: 0;
+  }
+
+  .JiiH li {
+    padding: 10px 40px;
+    color: blanchedalmond;
+    text-transform: uppercase;
+    font-family: 'Julius Sans One', sans-serif;
+    margin: 0 5px;
+    transition: .5s;
+    cursor: pointer;
+  }
 }
 
 #nav {
